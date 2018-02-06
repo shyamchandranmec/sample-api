089d8eabe3b5be9f4c173276291130be9f4127b2
Shyam Chandran <shyamchandranmec@gmail.com>
2018-02-06 16:58:33 +0530

    fix

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-var gcm = require("node-gcm");
+var FCM = require('fcm-node');
+
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
@@ -12,26 +13,34 @@ router.get('/', function (req, res, next) {
 });
 
 router.get('/fcm', function (req, res, next) {
-  var sender = new gcm.Sender("Your_Firebase_Cloud_Messaging_token");
 
-  var message = new gcm.Message({
+  var serverKey = 'AAAAnCJUYQg:APA91bEVTXBdKwGrE-lzMYyCEe5TUVaUm1KveQEKvEiLVsdojcnKcwAnqhBAtmE2BSMUMpW2FLQMaD2FJbMt5gtt3XiH1uUMfeUOf3eFN0kOgJGlqq1_fHqLtvgoMMND7oFVEZKgYus2';
+  var fcm = new FCM(serverKey);
+
+  var message = { //this may vary according to the message type (single recipient, multicast, topic, et cetera)
+    to: '/topics/news',
+    collapse_key: 'your_collapse_key',
+
     notification: {
-      title: "Hello World! ",
-      icon: "your_icon_name",
-      body: "Here is a notification's body."
+      title: 'Title of your push notification',
+      body: 'Body of your push notification'
     },
+
+    data: {  //you can send only notification or only data(or include both)
+      my_key: 'my value',
+      my_another_key: 'my another value'
+    }
+  };
+
+  fcm.send(message, function(err, response){
+    if (err) {
+      console.log("Something has gone wrong!", err);
+      res.json({failed: true})
+    } else {
+      console.log("Successfully sent with response: ", response);
+      res.json({success: true})
+    }
   });
-
-  var recipients = gcm.IRecipient = {to: "/topics/all"};
-
-
-  setTimeout(function () {
-    sender.sendNoRetry(message, recipients, function (err, response) {
-      if (err) console.error(err);
-      else console.log(response);
-    });
-    res.json({status: true})
-  }, 1000)
 
 });
 
